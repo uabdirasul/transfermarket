2ef4d192cd8d590ff46bd566e93496bf60ca243b
uabdirasulm <ubbiniyazov04@gmail.com>
2023-05-05 11:21:48 +0500

    init states

--- src/App.js
+++ src/App.js
@@ -3,6 +3,47 @@ import React, { Component } from "react";
 export default class App extends Component {
   constructor(props) {
     super();
+    this.state = {
+      players: [],
+    };
+  }
+
+  componentDidMount() {
+    const players = [
+      {
+        firstName: "Messi",
+        age: 35,
+        club: "Barcelona FC",
+        value: 160,
+      },
+      {
+        firstName: "Ronaldo",
+        age: 37,
+        club: "Al Nasr FC",
+        value: 200,
+      },
+      {
+        firstName: "Sadio Mane",
+        age: 29,
+        club: "Bayern Munich FC",
+        value: 60,
+      },
+      {
+        firstName: "Maguire",
+        age: 31,
+        club: "Manchester United",
+        value: 999,
+      },
+      {
+        firstName: "Neymar",
+        age: 31,
+        club: "PSG",
+        value: 222,
+      },
+    ];
+    this.setState({
+      players,
+    });
   }
 
   render() {
